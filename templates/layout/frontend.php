003482fc0ce85ba4656e3ca9479462d454fc17bf
Jagdeep Singh <jdecode@gmail.com>
2020-04-26 05:39:49 +0530

    Change paths

--- templates/layout/frontend.php
+++ templates/layout/frontend.php
@@ -5,7 +5,7 @@
     <title>
         <?= $this->fetch('title') ?> | Vue
     </title>
-    <link href="webroot/img/kode.png" type="image/png" rel="icon" />
+    <link href="img/kode.png" type="image/png" rel="icon" />
     <link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
@@ -13,7 +13,7 @@
     <div id="dashboard">
         {{ message }}
     </div>
-    <script src="vue/js/chunk-vendors.js"></script>
-    <script src="vue/js/app.js"></script>
+    <script src="webroot/vue/js/chunk-vendors.js"></script>
+    <script src="webroot/vue/js/app.js"></script>
 </body>
 </html>
